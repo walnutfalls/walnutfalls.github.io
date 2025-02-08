c670f8bc9c483031f971d740dc7431f6e85bcb7e
walnutfalls <walnutfalls@walnutfalls>
2025-02-07 20:59:38 -0800

    fix plugins

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   assetsInclude: ["**/*.png"],
   base: "https://walnutfalls.github.io/walnutfalls-site/",
 });
