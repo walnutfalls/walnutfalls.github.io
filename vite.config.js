633b7da1efc5af297c4f03e0a60e71bec4aa2ffc
walnutfalls <walnutfalls@walnutfalls>
2025-02-07 21:44:22 -0800

    fix site, remove js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [],
   base: "/walnutfalls-site/",
+  build: {
+        rollupOptions: {
+            input: {
+                main: "index.html",
+                styles: "src/style.css"
+            }
+        }
+    }
 });
