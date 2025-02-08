f213e0418ae8f8aae5439756f25a7be721ceec11
walnutfalls <walnutfalls@walnutfalls>
2025-02-07 20:47:12 -0800

    fix base

--- vite.conf.js
+++ vite.conf.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Base public path for the project
-  base: '/walnutfalls-site/',
+  base: 'https://walnutfalls.github.io/walnutfalls-site/',
   // Define build settings
   build: {
     outDir: 'dist', // Directory for the built files
