240ccf6e79b56257ce105c97a3a1171f531fe877
walnutfalls <walnutfalls@walnutfalls>
2025-04-03 12:28:08 -0700

    reinstate base for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [],
-  base: "/",
+  base: "/walnutfalls-site/",
   build: {
         rollupOptions: {
             input: {
